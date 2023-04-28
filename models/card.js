2cd23ee48a5caa79ca58781a2c91486c03a953b8
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-04-28 10:49:59 +0300

    fix: change type owner in cardSchema

--- models/card.js
+++ models/card.js
@@ -12,7 +12,8 @@ const cardSchema = new mongoose.Schema({
     required: true
   },
   owner: {
-    type: mongoose.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
     required: true
   },
   likes: {
