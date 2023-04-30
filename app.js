487842b10c0638bef88aac97c8e767b2c621ac7b
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-04-30 20:01:58 +0300

    fix: delete body parser

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
@@ -9,7 +8,7 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use((req, res, next) => {
   req.user = {
