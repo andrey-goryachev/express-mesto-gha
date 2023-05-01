b6da2a2a2a7f27471487a624750532befd1fc975
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-05-01 11:44:30 +0300

    fix: update avatar

--- controllers/users.js
+++ controllers/users.js
@@ -35,6 +35,7 @@ const updateProfile = (req, res) => {
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
+  console.log('🚀 ~ file: users.js:38 ~ updateAvatar ~ avatar:', avatar);
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => res.send({ user }))
     .catch((err) => handleErrors(err, res));
--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,13 @@ const {
   getUserById,
   createUser,
   updateProfile,
+  updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:id', getUserById);
 router.post('/', createUser);
 router.patch('/me', updateProfile);
-router.patch('/me/avatar', updateProfile);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
