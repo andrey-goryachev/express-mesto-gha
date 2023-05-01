8e784647f9e618cedffe12cf15e76fb61df77d4c
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-05-01 12:45:03 +0300

    fix: refactor eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: 'airbnb-base',
   rules: {
+    // это правило по заданию
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    // это правило отключает предупреждение, иначе тесты не проходят
+    'no-console': 'off',
   },
 };
