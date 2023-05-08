663302172fca3062966e0c9007281f08640cd3c2
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-05-08 11:50:46 +0300

    feat: change the createUser for the new userSchema

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const { handleErrors, NotFoundError } = require('../errors/errors');
 
@@ -19,9 +20,13 @@ const getUserById = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt.hash(password, 4)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
     .then((user) => res.status(201).send({ user }))
     .catch((err) => handleErrors(err, res));
 };
