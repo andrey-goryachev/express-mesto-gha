094bf01cb02f0fcfde28c90be4387f5f962408a5
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-05-14 21:41:48 +0300

    fix: validate the link in Joi, by a regular expression

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,11 @@ router.post('/signup', celebrate({
     password: Joi.string().required().min(2).max(30),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    // avatar: Joi.link(),
+    avatar: Joi.string().uri({
+      scheme: [
+        /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/gmi,
+      ],
+    }),
   }).unknown(true),
 }), createUser);
 router.use('/users', userRouter);
