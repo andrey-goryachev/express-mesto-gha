46a4b1b503fda47b7d3a88bb2272cf17808f39de
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-05-14 22:21:36 +0300

    fix: change validate the link in Joi

--- routes/index.js
+++ routes/index.js
@@ -25,11 +25,7 @@ router.post('/signup', celebrate({
     password: Joi.string().required().min(2).max(30),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri({
-      scheme: [
-        /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/gmi,
-      ],
-    }),
+    avatar: Joi.string().uri(),
   }).unknown(true),
 }), createUser);
 router.use('/users', userRouter);
