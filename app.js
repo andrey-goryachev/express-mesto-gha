7032cc3346a6507a786535202d10cef87c98d3ba
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-05-23 21:23:10 +0200

    feat: add logging

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const router = require('./routes/index');
 const { handleErrors } = require('./errors/errors');
 const { mongoPath } = require('./config');
+const {
+  requestLogger,
+  errorLogger,
+} = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -12,7 +16,9 @@ const app = express();
 mongoose.connect(mongoPath, { autoIndex: true });
 
 app.use(express.json());
+app.use(requestLogger);
 app.use(router);
+app.use(errorLogger);
 // noinspection JSCheckFunctionSignatures
 app.use(errors());
 app.use(handleErrors);
